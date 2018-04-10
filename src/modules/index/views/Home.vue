e8a53df02adb33fca758aa2e108343a504a1ce57
pengyingying <1403533052@qq.com>
2018-04-10 15:50:55 +0800

    跳转到后台系统

--- src/modules/index/views/Home.vue
+++ src/modules/index/views/Home.vue
@@ -22,50 +22,63 @@
         </router-link>
       </li>
       <li>
-        <router-link to="admin" class="el-button el-button-info">
+        <a href="javascript:void(0)" @click="toAdmin" class="el-button el-button-info">
           <span>后台管理</span>
-        </router-link>
+        </a>
+
       </li>
     </ul>
   </section>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: '登陆'
+  export default {
+    name: 'HelloWorld',
+    data () {
+      return {
+        msg: '登陆'
+      }
+    },
+    methods: {
+      toAdmin: function () {
+        window.location = '/modules/admin.html'
+      }
     }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  text-decoration: none;
-}
-.bg-purple-dark {
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  .bg-purple-dark {
     background: #99a9bf;
   }
+
   .bg-purple {
     background: #d3dce6;
   }
+
   .bg-purple-light {
     background: #e5e9f2;
   }
+
   .grid-content {
 
     min-height: 80px;
@@ -76,25 +89,28 @@ a {
     background-color: #f9fafc;
   }
 
-  .Menu1>li{
+  .Menu1 > li {
     float: left;
   }
-  .Menu1>li>a{
-        display: block;
-        width: 268px;
-        height: 80px;
-        margin: 20px;
-        padding: 0 15px;
-        line-height: 80px;
+
+  .Menu1 > li > a {
+    display: block;
+    width: 268px;
+    height: 80px;
+    margin: 20px;
+    padding: 0 15px;
+    line-height: 80px;
   }
- .h1-warp{
+
+  .h1-warp {
     padding-left: 2%;
     line-height: 80px;
     text-align: center;
- }
- .el-button-info{
-      color: #fff;
+  }
+
+  .el-button-info {
+    color: #fff;
     background-color: #50bfff;
     border-color: #50bfff;
- }
+  }
 </style>
